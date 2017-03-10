8937d5ed5870ab2a31ec36f27154d7a74d1bb46a
Kaius Reed <kmr117@case.edu>
2017-03-10 02:35:05 -0500

    Added functionality for controlling video playback
    -VideoPlayer contains methods for pausing, resuming, stopping, adjusting
    volume, and seeking positions in playback

--- UtilityMediaPlayer/src/VideoPlayer.java
+++ UtilityMediaPlayer/src/VideoPlayer.java
@@ -1,5 +1,6 @@
 import uk.co.caprica.vlcj.component.EmbeddedMediaPlayerComponent;
 import uk.co.caprica.vlcj.discovery.NativeDiscovery;
+import uk.co.caprica.vlcj.player.embedded.EmbeddedMediaPlayer;
 import uk.co.caprica.vlcj.player.media.Media;
 import javax.swing.*;
 import java.awt.event.*;
@@ -9,6 +10,7 @@ public class VideoPlayer
 {
 	private final JFrame frame;
 	private final EmbeddedMediaPlayerComponent mediaPlayerComponent;
+	private final EmbeddedMediaPlayer player;
 	
 	public VideoPlayer()
 	{
@@ -26,10 +28,19 @@ public class VideoPlayer
 				});
 		new NativeDiscovery().discover();
 		mediaPlayerComponent = new EmbeddedMediaPlayerComponent();
+		player = mediaPlayerComponent.getMediaPlayer();
 		
 	}
 	
-	//Reveals the video player and plays its selected media
+	public VideoPlayer(String filePath)
+	{
+		this();
+		player.prepareMedia(filePath);
+	}
+	
+	/**
+	 * Reveals the video player and plays its selected media
+	 */
 	public void open()
 	{
 		frame.setContentPane(mediaPlayerComponent);
@@ -42,15 +53,69 @@ public class VideoPlayer
 						new VideoPlayer();
 					}
 				});
-		mediaPlayerComponent.getMediaPlayer().playMedia("media libraries/video/kaius_presentation.mp4");
-	}
-	
-	//Assigns the video file to the video player
-	public void loadMedia(File videoFile)
-	{
+		
+		if(player.isPlayable())
+			playVideo();
 		
 	}
-
+	
+	/**
+	 * Plays the current video in the media player; Plays from paused position
+	 */
+	public void playVideo()
+	{
+		player.play();
+	}
+	
+	/**
+	 * Pauses playback of the current video
+	 */
+	public void pauseVideo()
+	{
+		player.pause();
+		
+	}
+	
+	/**
+	 * Stops playback of the current video
+	 */
+	public void stopVideo()
+	{
+		player.stop();
+	}
+	
+	/**
+	 * Assigns the video file to the video player
+	 * @param filePath The local file path of the video to be played
+	 */
+	public void loadVideo(String filePath)
+	{
+		player.prepareMedia(filePath);
+	}
+	
+	/**
+	 * Play video from a certain position
+	 * @param time Time in milliseconds to play from
+	 */
+	public void seekVideo(long time)
+	{
+		if(player.isSeekable())
+			player.setTime(time);
+	}
+	
+	/**
+	 * Change the volume of the current playing video
+	 * @param volumePercentage The new percentage to set the volume to
+	 */
+	public void changeVolume(int volumePercentage)
+	{
+		player.setVolume(volumePercentage);
+	}
+	
+	/**
+	 * Get the JFrame of this player
+	 * @return
+	 */
 	public JFrame getFrame()
 	{
 		return frame;
@@ -58,7 +123,17 @@ public class VideoPlayer
 	
 	public static void main(String[] args)
 	{
-		VideoPlayer v = new VideoPlayer();
+		//Testing stuff
+		VideoPlayer v = new VideoPlayer("media libraries/video/kaius_presentation.mp4");
 		v.open();
+		
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			Thread.currentThread().interrupt();
+		}
+		
+		v.pauseVideo();
 	}
 }
