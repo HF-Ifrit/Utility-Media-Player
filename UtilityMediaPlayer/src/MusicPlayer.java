f2c2550e7902999a25489afb0f0faacac02ca240
Danny Miles <dmiles4824@msn.com>
2017-03-10 05:35:46 -0500

    Implemented volume features into MusicPlayer, and updated signature of Player interface
    Signed-off-by: Danny Miles <dmiles4824@msn.com>

--- UtilityMediaPlayer/src/MusicPlayer.java
+++ UtilityMediaPlayer/src/MusicPlayer.java
@@ -1,25 +1,35 @@
 import java.io.*;
 import java.util.ArrayList;
 
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.Media;
 import javafx.application.Application;
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class MusicPlayer extends Application {
+public class MusicPlayer extends Application implements Player {
 	
 	boolean songLoaded;
 	boolean isPaused;
 	MediaPlayer player;
+	Slider volume;
 	
 	/*The method that starts when the MusicPlayer is run. Will be delegated to a controller later in development;
 	 * for now, this allows the MusicPlayer to function as a stand-alone application.(non-Javadoc)
@@ -29,6 +39,7 @@ public class MusicPlayer extends Application {
 	public void start(Stage primaryStage) {
 		player = null;
 		songLoaded = false;
+		volume = null;
 		primaryStage.setTitle("Music Player");
 		GridPane grid = new GridPane();
 		grid.setAlignment(Pos.CENTER);
@@ -44,9 +55,18 @@ public class MusicPlayer extends Application {
 					alternatePlayback(player);
 				}
 				else {
-					System.out.println("PLAY THAT FUCKIN TRAACK");
+					System.out.println("Song is playing.");
 					open("media libraries/test.mp3");
 					songLoaded = true;
+				}
+			}
+		});
+		
+		volume = createSlider("Volume: ", 2, 2, grid);
+		volume.valueProperty().addListener(new InvalidationListener() {
+			public void invalidated(Observable ov) {
+				if (volume.isValueChanging()) {
+					volumeChange(player);
 				}
 			}
 		});
@@ -54,6 +74,7 @@ public class MusicPlayer extends Application {
 		primaryStage.setScene(new Scene(grid, 300, 275));
 		primaryStage.show();
 	}
+	@Override
 	/*Opens the specified music file and loads it into the player
 	 * @param fileName The name of the desired music file
 	 */
@@ -62,6 +83,7 @@ public class MusicPlayer extends Application {
 		Media media = new Media(uri);
 		MediaPlayer musicPlayer = new MediaPlayer(media);
 		this.player = musicPlayer;
+		player.setVolume(1.0);
 		musicPlayer.play();
 		}
 	
@@ -69,7 +91,7 @@ public class MusicPlayer extends Application {
 	 * from 'playing' to 'paused' and vice versa.
 	 * @param musicPlayer The MediaPlayer associated with the current track
 	 */
-	public void alternatePlayback(MediaPlayer musicPlayer) {
+	private void alternatePlayback(MediaPlayer musicPlayer) {
 		if (songLoaded) {
 			if (isPaused) {
 				musicPlayer.play();
@@ -81,6 +103,38 @@ public class MusicPlayer extends Application {
 			}
 		}
 	}
+	
+	private void volumeChange(MediaPlayer musicPlayer) {
+		player.setVolume(volume.getValue());
+	}
+	
+	private void changePosition(MediaPlayer musicPlayer) {
+		int y = 1;
+	}
+	
+	private void skipPlayback(MediaPlayer musicPlayer) {
+		int z = 1;
+	}
+	
+	/*The following are methods that serve as the implementation of the Player interface. They call the appropriate methods in the MusicPlayer class. */
+	
+	public void alternatePlayback() {
+		alternatePlayback(player);
+	}
+	
+	public void volumeChange() {
+		volumeChange(player);
+	}
+	
+	public void changePosition() {
+		changePosition(player);
+	}
+	
+	public void skipPlayback() {
+		skipPlayback(player);
+	}
+
+	
 	
 	/*Helper method that generates a Button and adds it to the grid. Used for setup.
 	 * @param buttonName The text displayed on the button
@@ -96,6 +150,17 @@ public class MusicPlayer extends Application {
 		box.getChildren().add(button);
 		grid.add(box,  column, row);
 		return button;
+	}
+	
+	private Slider createSlider(String name,int column, int row, GridPane grid) {
+		Slider volumeSlider = new Slider(0, 1, 1);
+		Label volumeLabel = new Label(name);
+		grid.add(volumeLabel, column, row - 1);
+		volumeSlider.setPrefWidth(70);
+		volumeSlider.setMaxWidth(Region.USE_PREF_SIZE);
+		volumeSlider.setMinWidth(30);
+		grid.add(volumeSlider, column, row);
+		return volumeSlider;
 	}
 	
 	public static void main(String[] args) {
--- UtilityMediaPlayer/src/Player.java
+++ UtilityMediaPlayer/src/Player.java
@@ -1,5 +1,9 @@
 
 public interface Player 
 {
-	public void open();
+	public void open(String fileName);
+	public void volumeChange();
+	public void alternatePlayback();
+	public void skipPlayback();
+	public void changePosition();
 }
