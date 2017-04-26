8412afbc3bdcf37e9183ca0418fbe2fad93630a3
Abel Keith <abelkeith@gmail.com>
2017-04-25 20:51:43 -0400

    make window default to maximized, minus toolbars

--- UtilityMediaPlayer/src/MainFrame.java
+++ UtilityMediaPlayer/src/MainFrame.java
@@ -124,6 +124,8 @@ public class MainFrame extends JFrame {
 	private static void createAndShowGUI() {
         //Create and set up the window.
         JFrame displayFrame = new JFrame();
+        displayFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+
         displayFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
  
         //Create and set up the content pane.
@@ -624,6 +626,8 @@ public class MainFrame extends JFrame {
 		public JFrame createAndShowGUI() {
 	        //Create and set up the window.
 	        JFrame frame = new JFrame("UMP Controller");
+	        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+
 	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	 
 	        //Create and set up the content pane.
