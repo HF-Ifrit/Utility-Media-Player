a550dda2f3c52da18bb643dc363069acacbb140a
Andrew Su <als211@case.edu>
2017-04-27 20:43:10 -0400

    fixing playlists
    Signed-off-by: Andrew Su <als211@case.edu>

--- UtilityMediaPlayer/src/MainFrame.java
+++ UtilityMediaPlayer/src/MainFrame.java
@@ -326,9 +326,7 @@ public class MainFrame extends JFrame {
 	private static JList<String> createPlayListView(MainFrame mainFrame){
 		JList<String> list;
 		list = new JList<String>();
-		//TODO add file names here
 		ArrayList<String> playList = new ArrayList<String>();
-		playList.add("Play Lists: f");
 		
 		playListModel = new DefaultListModel<String>();
 		
@@ -774,6 +772,7 @@ public class MainFrame extends JFrame {
 				tempmode = parseFileType(filename);
 			}
 			else{
+				listMode = Mode.EMPTY;
 				mode = Mode.EMPTY;
 				return;
 			}
